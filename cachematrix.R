04c9351c1b401495437666acc160bfcf4fb3cc08
orfalchechor <bidnightsnaq@outlook.com>
2016-06-04 19:45:04 +0200

    Course Assignment Programming Assignment 2: Lexical Scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Description:
+##   makeCacheMatrix create an object contain a matrix, which cacheSolve
+##   can consume and calculate its inverse (provided the matrix invertible).
+##   Made possible by example code provided by course.org from repo rdpeng.
+##
+## Example:
+##    z <- makeCacheMatrix()
+##    z$set( c(1,-1,4,6), 2 )
+##    cacheSolve(z)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## function: makeCacheMatrix( x=matrix() )
+##     initialized with an empty matrix
+##
+## $set( x, ... )
+##    accepts data to populate a matrix 
+##    initializes the cached inverse
+## $get()
+##    returns the matrix
+## $setinverse( x )
+##    private function, sets the calculated inverse
+## $getinverse()
+##    returns the calculated inverse
 
+makeCacheMatrix <- function( x = matrix() ) {
+   i <- NULL
+   set <- function(y, ... ) {
+      x <<- matrix( y, ... )
+      i <<- NULL
+   }
+   get <- function() x
+   setinverse <- function(solve) i <<- solve
+   getinverse <- function() i
+   list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## function: cacheSolve( x, ... )
+##    accepts a makeCacheMatrix and retrieves
+##    any cached value for the inversed matrix.
+##    When none exist, it retrieves the data from
+##    makeCacheMatrix, calculates the inverse and
+#     stores it in makeCacheMatrix using the $set method.
+##
+##    returns : inverse of the makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   i <- x$getinverse()
+   if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+   }
+   data <- x$get()
+   i <- solve(data, ...)
+   x$setinverse(i)
+   i
 }
